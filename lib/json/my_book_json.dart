1ef079a0847b3e1c2cc0fd77b18bf155816e6687
zhangsunjiankun <zhangsunjiankun@dingtalk.com>
2024-09-22 23:30:32 +0800

    完成我的书籍

--- lib/json/my_book_json.dart
+++ lib/json/my_book_json.dart
@@ -1,17 +1,6 @@
 const List continueReadingJson = [
   {
-    "img": "https://covers.openlibrary.org/b/id/10601397-L.jpg",
-    "price": "3.45",
-    "title": "Danny the Champion of the World",
-    "sub_title": "Danny the Champion of the World",
-    "author_name": "Roald Dahl",
-    "rate": 3.2,
-    "favourite": false,
-    "page": "100",
-    "percentage": 65.0
-  },
-  {
-    "img": "https://covers.openlibrary.org/w/id/149650-M.jpg",
+    "img": "lib/images/149650-M.jpg",
     "price": "5.99",
     "title": "Cell",
     "sub_title": "A Novel",
@@ -19,10 +8,12 @@ const List continueReadingJson = [
     "rate": 4.5,
     "favourite": false,
     "page": "90",
-    "percentage": 30.0
+    "percentage": 30.0,
+    "about":
+        "Moby-Dick by Herman Melville: Moby-Dick by Herman Melville is a profound exploration of obsession, vengeance, and the human condition. The novel is narrated by Ishmael, a sailor who joins the whaling ship Pequod, captained by the enigmatic and monomaniacal Ahab. Ahab is consumed by his desire for revenge against Moby Dick, a colossal white whale that had previously destroyed Ahab’s ship and left him with a mutilated leg. As the Pequod sails across the vast oceans, the crew encounters various challenges, both external and internal, that test their limits. The novel delves deep into philosophical and existential themes, using the voyage as a metaphor for the human struggle against an indifferent universe. Melville’s rich language, complex characters, and allegorical depth have cemented Moby-Dick as a cornerstone of American literature."
   },
   {
-    "img": "https://covers.openlibrary.org/b/id/7081826-L.jpg",
+    "img": "lib/images/7081826-L.jpg",
     "price": "5.99",
     "title": "Chemistry",
     "sub_title": "4th ed.",
@@ -30,39 +21,135 @@ const List continueReadingJson = [
     "rate": 3.0,
     "favourite": false,
     "page": "150",
-    "percentage": 85.0
+    "percentage": 85.0,
+    "about":
+        "Crime and Punishment by Fyodor Dostoevsky: Crime and Punishment by Fyodor Dostoevsky is a psychological drama that explores the moral dilemmas and inner turmoil of its protagonist, Rodion Raskolnikov. Set in the gritty streets of 19th-century St. Petersburg, the novel follows Raskolnikov, an impoverished former student, as he devises a plan to murder a pawnbroker for her money. Believing that he can transcend ordinary moral laws, Raskolnikov commits the crime, but he is soon plagued by guilt and paranoia. The novel examines themes of redemption, the nature of evil, and the psychological impact of crime on the human psyche. Dostoevsky's exploration of Raskolnikov's fractured mind and his ultimate quest for redemption offers a profound insight into the complexities of morality and justice, making it a cornerstone of Russian literature."
   },
+  {
+    "img": "lib/images/0010601397-L.jpg",
+    "price": "3.45",
+    "title": "Danny the Champion of the World",
+    "sub_title": "Danny the Champion of the World",
+    "author_name": "Roald Dahl",
+    "rate": 3.2,
+    "favourite": false,
+    "page": "100",
+    "percentage": 65.0,
+    "about":
+        "The Great Gatsby by F. Scott Fitzgerald: F. Scott Fitzgerald’s The Great Gatsby is a poignant reflection on the American Dream and the moral decay of society during the Roaring Twenties. The novel is narrated by Nick Carraway, a young man who moves to Long Island and becomes entangled in the world of the wealthy and mysterious Jay Gatsby. Gatsby is famous for his lavish parties, but beneath his opulent lifestyle lies an unrequited love for Daisy Buchanan, Nick’s cousin. As Gatsby’s obsession with rekindling his romance with Daisy intensifies, the novel exposes the hollow pursuit of wealth and status, and the disillusionment that follows. Fitzgerald's lyrical prose and sharp social commentary paint a vivid picture of a society consumed by greed, ambition, and the pursuit of an unattainable ideal, making The Great Gatsby a defining work of American literature."
+  },
+  {
+    "img": "lib/images/149650-M.jpg",
+    "price": "5.99",
+    "title": "Cell",
+    "sub_title": "A Novel",
+    "author_name": "Stephen King",
+    "rate": 4.5,
+    "favourite": false,
+    "page": "90",
+    "percentage": 30.0,
+    "about":
+        "1984 by George Orwell: George Orwell's 1984 is a dystopian novel that presents a chilling portrayal of a totalitarian society under the omnipresent surveillance of Big Brother. The story is set in a grim future where the Party, led by Big Brother, exercises absolute control over every aspect of life, including thought and behavior. The protagonist, Winston Smith, works at the Ministry of Truth, where he alters historical records to fit the Party's propaganda. Despite the oppressive regime, Winston harbors rebellious thoughts and begins a forbidden love affair, leading him to question the Party’s authority. The novel explores themes of censorship, the manipulation of truth, and the loss of individuality. Orwell’s stark warning about the dangers of totalitarianism and the erosion of personal freedoms has made 1984 a powerful and enduring cautionary tale that resonates deeply with modern readers."
+  },
+  {
+    "img": "lib/images/7081826-L.jpg",
+    "price": "5.99",
+    "title": "Chemistry",
+    "sub_title": "4th ed.",
+    "author_name": "Karen Timberlake",
+    "rate": 3.0,
+    "favourite": false,
+    "page": "150",
+    "percentage": 85.0,
+    "about":
+        "1984 by George Orwell: George Orwell's 1984 is a dystopian novel that presents a chilling portrayal of a totalitarian society under the omnipresent surveillance of Big Brother. The story is set in a grim future where the Party, led by Big Brother, exercises absolute control over every aspect of life, including thought and behavior. The protagonist, Winston Smith, works at the Ministry of Truth, where he alters historical records to fit the Party's propaganda. Despite the oppressive regime, Winston harbors rebellious thoughts and begins a forbidden love affair, leading him to question the Party’s authority. The novel explores themes of censorship, the manipulation of truth, and the loss of individuality. Orwell’s stark warning about the dangers of totalitarianism and the erosion of personal freedoms has made 1984 a powerful and enduring cautionary tale that resonates deeply with modern readers."
+  },
+  {
+    "img": "lib/images/0010601397-L.jpg",
+    "price": "3.45",
+    "title": "Danny the Champion of the World",
+    "sub_title": "Danny the Champion of the World",
+    "author_name": "Roald Dahl",
+    "rate": 3.2,
+    "favourite": false,
+    "page": "100",
+    "percentage": 65.0,
+    "about":
+        "1984 by George Orwell: George Orwell's 1984 is a dystopian novel that presents a chilling portrayal of a totalitarian society under the omnipresent surveillance of Big Brother. The story is set in a grim future where the Party, led by Big Brother, exercises absolute control over every aspect of life, including thought and behavior. The protagonist, Winston Smith, works at the Ministry of Truth, where he alters historical records to fit the Party's propaganda. Despite the oppressive regime, Winston harbors rebellious thoughts and begins a forbidden love affair, leading him to question the Party’s authority. The novel explores themes of censorship, the manipulation of truth, and the loss of individuality. Orwell’s stark warning about the dangers of totalitarianism and the erosion of personal freedoms has made 1984 a powerful and enduring cautionary tale that resonates deeply with modern readers."
+  }
 ];
 
 const List unReadJson = [
   {
-    "img": "https://covers.openlibrary.org/b/id/12860069-L.jpg",
-    "price": "2.50",
-    "title": "I'm Glad My Mom Died",
-    "sub_title": "I'm Glad My Mom Died",
-    "author_name": "Jennette McCurdy",
-    "rate": 4.0,
-    "favourite": false,
-    "page": "200"
-  },
-  {
-    "img": "https://covers.openlibrary.org/b/id/12842027-L.jpg",
+    "img": "lib/images/0012842027-L.jpg",
     "price": "5.99",
     "title": "The Love Hypothesis",
     "sub_title": "The Love Hypothesis",
     "author_name": "Ali Hazelwood",
     "rate": 3.5,
     "favourite": false,
-    "page": "150"
+    "page": "150",
+    "about":
+        "1984 by George Orwell: George Orwell's 1984 is a dystopian novel that presents a chilling portrayal of a totalitarian society under the omnipresent surveillance of Big Brother. The story is set in a grim future where the Party, led by Big Brother, exercises absolute control over every aspect of life, including thought and behavior. The protagonist, Winston Smith, works at the Ministry of Truth, where he alters historical records to fit the Party's propaganda. Despite the oppressive regime, Winston harbors rebellious thoughts and begins a forbidden love affair, leading him to question the Party’s authority. The novel explores themes of censorship, the manipulation of truth, and the loss of individuality. Orwell’s stark warning about the dangers of totalitarianism and the erosion of personal freedoms has made 1984 a powerful and enduring cautionary tale that resonates deeply with modern readers."
   },
   {
-    "img": "https://covers.openlibrary.org/b/id/402210-L.jpg",
+    "img": "lib/images/0012860069-L.jpg",
+    "price": "2.50",
+    "title": "I'm Glad My Mom Died",
+    "sub_title": "I'm Glad My Mom Died",
+    "author_name": "Jennette McCurdy",
+    "rate": 4.0,
+    "favourite": false,
+    "page": "200",
+    "about":
+        "1984 by George Orwell: George Orwell's 1984 is a dystopian novel that presents a chilling portrayal of a totalitarian society under the omnipresent surveillance of Big Brother. The story is set in a grim future where the Party, led by Big Brother, exercises absolute control over every aspect of life, including thought and behavior. The protagonist, Winston Smith, works at the Ministry of Truth, where he alters historical records to fit the Party's propaganda. Despite the oppressive regime, Winston harbors rebellious thoughts and begins a forbidden love affair, leading him to question the Party’s authority. The novel explores themes of censorship, the manipulation of truth, and the loss of individuality. Orwell’s stark warning about the dangers of totalitarianism and the erosion of personal freedoms has made 1984 a powerful and enduring cautionary tale that resonates deeply with modern readers."
+  },
+  {
+    "img": "lib/images/402210-L.jpg",
     "price": "2.20",
     "title": "The 48 Laws of Power",
     "sub_title": "The 48 Laws of Power",
     "author_name": "Robert Greene",
     "rate": 4.07,
     "favourite": false,
-    "page": "60"
+    "page": "60",
+    "about":
+        "1984 by George Orwell: George Orwell's 1984 is a dystopian novel that presents a chilling portrayal of a totalitarian society under the omnipresent surveillance of Big Brother. The story is set in a grim future where the Party, led by Big Brother, exercises absolute control over every aspect of life, including thought and behavior. The protagonist, Winston Smith, works at the Ministry of Truth, where he alters historical records to fit the Party's propaganda. Despite the oppressive regime, Winston harbors rebellious thoughts and begins a forbidden love affair, leading him to question the Party’s authority. The novel explores themes of censorship, the manipulation of truth, and the loss of individuality. Orwell’s stark warning about the dangers of totalitarianism and the erosion of personal freedoms has made 1984 a powerful and enduring cautionary tale that resonates deeply with modern readers."
+  },
+  {
+    "img": "lib/images/0012842027-L.jpg",
+    "price": "5.99",
+    "title": "The Love Hypothesis",
+    "sub_title": "The Love Hypothesis",
+    "author_name": "Ali Hazelwood",
+    "rate": 3.5,
+    "favourite": false,
+    "page": "150",
+    "about":
+        "1984 by George Orwell: George Orwell's 1984 is a dystopian novel that presents a chilling portrayal of a totalitarian society under the omnipresent surveillance of Big Brother. The story is set in a grim future where the Party, led by Big Brother, exercises absolute control over every aspect of life, including thought and behavior. The protagonist, Winston Smith, works at the Ministry of Truth, where he alters historical records to fit the Party's propaganda. Despite the oppressive regime, Winston harbors rebellious thoughts and begins a forbidden love affair, leading him to question the Party’s authority. The novel explores themes of censorship, the manipulation of truth, and the loss of individuality. Orwell’s stark warning about the dangers of totalitarianism and the erosion of personal freedoms has made 1984 a powerful and enduring cautionary tale that resonates deeply with modern readers."
+  },
+  {
+    "img": "lib/images/0012860069-L.jpg",
+    "price": "2.50",
+    "title": "I'm Glad My Mom Died",
+    "sub_title": "I'm Glad My Mom Died",
+    "author_name": "Jennette McCurdy",
+    "rate": 4.0,
+    "favourite": false,
+    "page": "200",
+    "about":
+        "1984 by George Orwell: George Orwell's 1984 is a dystopian novel that presents a chilling portrayal of a totalitarian society under the omnipresent surveillance of Big Brother. The story is set in a grim future where the Party, led by Big Brother, exercises absolute control over every aspect of life, including thought and behavior. The protagonist, Winston Smith, works at the Ministry of Truth, where he alters historical records to fit the Party's propaganda. Despite the oppressive regime, Winston harbors rebellious thoughts and begins a forbidden love affair, leading him to question the Party’s authority. The novel explores themes of censorship, the manipulation of truth, and the loss of individuality. Orwell’s stark warning about the dangers of totalitarianism and the erosion of personal freedoms has made 1984 a powerful and enduring cautionary tale that resonates deeply with modern readers."
+  },
+  {
+    "img": "lib/images/402210-L.jpg",
+    "price": "2.20",
+    "title": "The 48 Laws of Power",
+    "sub_title": "The 48 Laws of Power",
+    "author_name": "Robert Greene",
+    "rate": 4.07,
+    "favourite": false,
+    "page": "60",
+    "about":
+        "1984 by George Orwell: George Orwell's 1984 is a dystopian novel that presents a chilling portrayal of a totalitarian society under the omnipresent surveillance of Big Brother. The story is set in a grim future where the Party, led by Big Brother, exercises absolute control over every aspect of life, including thought and behavior. The protagonist, Winston Smith, works at the Ministry of Truth, where he alters historical records to fit the Party's propaganda. Despite the oppressive regime, Winston harbors rebellious thoughts and begins a forbidden love affair, leading him to question the Party’s authority. The novel explores themes of censorship, the manipulation of truth, and the loss of individuality. Orwell’s stark warning about the dangers of totalitarianism and the erosion of personal freedoms has made 1984 a powerful and enduring cautionary tale that resonates deeply with modern readers."
   },
 ];
--- lib/pages/my_books_page.dart
+++ lib/pages/my_books_page.dart
@@ -1,7 +1,10 @@
 import 'package:e_book_app/components/search_box.dart';
 import 'package:e_book_app/components/shop_bag_with_message.dart';
+import 'package:e_book_app/json/my_book_json.dart';
+import 'package:e_book_app/pages/book_detail_page.dart';
 import 'package:e_book_app/theme/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:logger/logger.dart';
 
 final _logger = Logger();
@@ -38,12 +41,231 @@ class _MyBooksPageState extends State<MyBooksPage> {
       child: Padding(
         padding: const EdgeInsets.all(15.0),
         child: Column(
-          children: [
-            getSearchBox(),
-          ],
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [getSearchBox(), getContinueReading(), getUnread()],
         ),
       ),
     ));
+  }
+
+  Widget getUnread() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(
+          height: 15,
+        ),
+        Text(
+          "Unread(${unReadJson.length})",
+          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ...unReadJson.map((item) => GestureDetector(
+                    onTap: () => {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => BookDetailPage(
+                                  img: item['img'],
+                                  price: item['price'],
+                                  title: item['title'],
+                                  subTitle: item['sub_title'],
+                                  authorName: item['author_name'],
+                                  rate: item['rate'],
+                                  favourite: item['favourite'],
+                                  page: int.parse(item['page']),
+                                  about: item['about'])))
+                    },
+                    child: Container(
+                      height: 105,
+                      margin: const EdgeInsets.only(bottom: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 80,
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage(item['img']),
+                                    fit: BoxFit.cover),
+                                color: black.withOpacity(0.4),
+                                borderRadius: BorderRadius.circular(5)),
+                          ),
+                          const SizedBox(
+                            width: 12,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text(
+                                item['title'],
+                                style: const TextStyle(
+                                    fontSize: 15, fontWeight: FontWeight.w600),
+                              ),
+                              Text(
+                                item['sub_title'],
+                                style: TextStyle(
+                                    fontSize: 13,
+                                    color: black.withOpacity(0.4)),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  RatingBar.builder(
+                                      maxRating: 5,
+                                      minRating: 1,
+                                      itemCount: 5,
+                                      itemSize: 13,
+                                      allowHalfRating: true,
+                                      direction: Axis.horizontal,
+                                      wrapAlignment: WrapAlignment.start,
+                                      initialRating: item['rate'],
+                                      itemPadding:
+                                          const EdgeInsets.only(right: 2),
+                                      itemBuilder: (context, index) {
+                                        return const Icon(
+                                          Icons.star,
+                                          color: danger,
+                                        );
+                                      },
+                                      onRatingUpdate: (rating) {}),
+                                  Text(
+                                    "(${item['rate']})",
+                                    style: const TextStyle(color: danger),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ))
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget getContinueReading() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        const SizedBox(
+          height: 15,
+        ),
+        Text(
+          "Continue reading(${continueReadingJson.length})",
+          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+        ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              ...continueReadingJson.map((item) => Container(
+                    width: 120,
+                    margin:
+                        const EdgeInsets.only(right: 10, top: 10, bottom: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        GestureDetector(
+                          onTap: () => {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => BookDetailPage(
+                                        img: item['img'],
+                                        price: item['price'],
+                                        title: item['title'],
+                                        subTitle: item['sub_title'],
+                                        authorName: item['author_name'],
+                                        rate: item['rate'],
+                                        favourite: item['favourite'],
+                                        page: int.parse(item['page']),
+                                        about: item['about'])))
+                          },
+                          child: Container(
+                            height: 160,
+                            decoration: BoxDecoration(
+                                color: black.withOpacity(0.1),
+                                image: DecorationImage(
+                                    image: AssetImage(item['img']),
+                                    fit: BoxFit.cover),
+                                borderRadius: BorderRadius.circular(5)),
+                          ),
+                        ),
+                        Text(
+                          item['title'],
+                          maxLines: 1,
+                          style: const TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w600,
+                              overflow: TextOverflow.ellipsis),
+                        ),
+                        Text(
+                          item['sub_title'],
+                          maxLines: 1,
+                          style: TextStyle(
+                              color: black.withOpacity(0.4),
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
+                              overflow: TextOverflow.ellipsis),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Flexible(
+                              child: LayoutBuilder(
+                                builder: (context, constraints) => Stack(
+                                  children: [
+                                    Container(
+                                      height: 3,
+                                      decoration: BoxDecoration(
+                                          color: black.withOpacity(0.2)),
+                                    ),
+                                    Container(
+                                      height: 3,
+                                      width: item['percentage'] /
+                                          100 *
+                                          constraints.maxWidth,
+                                      decoration:
+                                          const BoxDecoration(color: primary),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              "${item['percentage']}%",
+                              style: TextStyle(
+                                  fontSize: 12,
+                                  color: black.withOpacity(0.4),
+                                  letterSpacing: 0.01),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ))
+            ],
+          ),
+        )
+      ],
+    );
   }
 
   Widget getSearchBox() {
